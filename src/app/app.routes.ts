a6713c6e747535d6342cbd747fac9d9f6d624e3f
Steffen Schanze <steffenjoachim.schanze@gmail.com>
2024-12-17 16:32:45 +0100

    imprint and data-protection added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'impressum', component: ImprintComponent },
+    { path: 'datenschutz', component: DataProtectionComponent }
 ];
---
+++ src/app/data-protection/data-protection.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-protection',
+  imports: [],
+  templateUrl: './data-protection.component.html',
+  styleUrl: './data-protection.component.css'
+})
+export class DataProtectionComponent {
+
+}
